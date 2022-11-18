bb67f3ad0e19a0a944fc3169f8db04ddf5c5abc9
Eugénie <eugenie.b24@hotmail.fr>
2022-11-18 10:33:44 +0000

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    <h1>Weather App</h1>
-    <footer>
-      <p>
-        Coded by <a href="https://deft-banoffee-8f140b.netlify.app/" target="_blank" rel="noreferrer noopener">Eugénie BvN</a>, and open source on <a href="https://github.com/Shiimymy/react-weather-project" target="_blank" rel="noreferrer noopener">Github</a>
-      </p>
-    </footer>
+      <div class="container">
+        <h1>Weather App</h1>
+        <footer>
+          <p>
+            Coded by <a href="https://deft-banoffee-8f140b.netlify.app/" target="_blank" rel="noreferrer noopener">Eugénie BvN</a>, and open source on <a href="https://github.com/Shiimymy/react-weather-project" target="_blank" rel="noreferrer noopener">Github</a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
