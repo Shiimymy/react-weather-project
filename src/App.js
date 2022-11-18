78a556b9aad96a81ed20d1fde6980de890878822
Eugénie <eugenie.b24@hotmail.fr>
2022-11-18 10:01:46 +0000

    added website and Github links

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+    <h1>Weather App</h1>
+    <footer>
+      <p>
+        Coded by <a href="https://deft-banoffee-8f140b.netlify.app/" target="_blank" rel="noreferrer noopener">Eugénie BvN</a>, and open source on <a href="https://github.com/Shiimymy/react-weather-project" target="_blank" rel="noreferrer noopener">Github</a>
+      </p>
+    </footer>
     </div>
   );
 }
