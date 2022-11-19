3669eb6a3791b91fdcd2359565eac525253ffa6a
Eugénie <eugenie.b24@hotmail.fr>
2022-11-19 13:40:32 +0000

    update Footer position

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="Dublin"/>
       </div>
-      <footer>
-          <p>
-            Coded by <a href="https://deft-banoffee-8f140b.netlify.app/" target="_blank" rel="noreferrer noopener" className="infoLinks">Eugénie BvN</a>, and open source on <a href="https://github.com/Shiimymy/react-weather-project" target="_blank" rel="noreferrer noopener" className="infoLinks">Github</a>
-          </p>
-        </footer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,14 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div>
     <App />
+    <footer>
+          <p>
+            Coded by <a href="https://deft-banoffee-8f140b.netlify.app/" target="_blank" rel="noreferrer noopener" className="infoLinks">Eugénie BvN</a>, and open source on <a href="https://github.com/Shiimymy/react-weather-project" target="_blank" rel="noreferrer noopener" className="infoLinks">Github</a>
+          </p>
+        </footer>
+    </div>
   </React.StrictMode>
 );
 
