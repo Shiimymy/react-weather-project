6988fdd04063aa9cec2c5c3b6d68c95c89efea73
Eugénie <eugenie.b24@hotmail.fr>
2022-11-19 13:17:03 +0000

    added Fahrenheit conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate.js";
+import WeatherTemperature from "./WeatherTemperature.js"
 
 export default function WeatherInfo (props) {
     return (
@@ -13,8 +14,7 @@ export default function WeatherInfo (props) {
                 {" "}
                 <img src={props.data.iconUrl} alt="SheCodes API icons" id="todayIcon"/>
                 <p>{props.data.description}</p>
-                <span id="todayTemp">{props.data.temperature}</span>
-                °C
+                <WeatherTemperature celsius={props.data.temperature}/>
             </h2>
             <h6>
                 Humidity : <span id="humidity">{props.data.humidity}</span> % | Wind :{" "}
---
+++ src/WeatherTemperature.js
@@ -0,0 +1,40 @@
+import React, {useState} from "react";
+
+export default function WeatherTemperature(props){
+    const [unit, setUnit]= useState("celsius");
+
+    function convertToFahrenheit(event) {
+        event.preventDefault();
+        setUnit("fahrenheit");
+    }
+
+     function convertToCelsius(event) {
+        event.preventDefault();
+        setUnit("celsius");
+    }
+
+    if (unit === "celsius") {
+    return (
+        <span id="todayTemp">{props.celsius}
+            <span className="unit">
+                °C
+            </span> |  <span className="unit">
+                <a href="/" onClick={convertToFahrenheit}>°F</a>
+            </span>
+        </span>
+    );
+    }  else {
+        let fahrenheit = (props.celsius * 9/5) + 32;
+
+        return (
+        <span id="todayTemp">
+            {Math.round(fahrenheit)}
+            <span className="unit">
+                <a href="/" onClick={convertToCelsius}>°C</a>
+            </span> |  <span className="unit">
+                °F
+            </span>
+        </span>
+    );
+    }
+}
